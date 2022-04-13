56b1bd6642c4a65e481da1fb81104e3735284eef
Collin Jones <collin.g.jones34@gmail.com>
2022-04-13 11:52:24 -0500

    adding more walk through code

--- src/main/java/com/example/restblog/data/Post.java
+++ src/main/java/com/example/restblog/data/Post.java
@@ -1,12 +1,17 @@
 package com.example.restblog.data;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Post {
     private Long id;
     private String title;
     private String content;
+
+    public Post(Long postId, String s) {
+    }
 }

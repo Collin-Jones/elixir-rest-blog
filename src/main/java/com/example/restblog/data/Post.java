2dea5991cf316f3f9552afd2f2591934027667fc
Collin Jones <collin.g.jones34@gmail.com>
2022-04-13 11:15:57 -0500

    commiting what I have done so far with spring

--- src/main/java/com/example/restblog/data/Post.java
+++ src/main/java/com/example/restblog/data/Post.java
@@ -1,30 +1,12 @@
 package com.example.restblog.data;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
-@Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name="posts")
 public class Post {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnore
-    private long id;
-    @NotNull
+    private Long id;
     private String title;
-    @NotNull
     private String content;
-    @NotNull
-    @ManyToOne(fetch = FetchType.EAGER)
-    @ToString.Exclude
-    private User user;
-
 }

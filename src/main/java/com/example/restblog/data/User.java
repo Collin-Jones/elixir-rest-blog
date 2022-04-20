e9e488de06d0932bf7c5d9814de9f7e52f248afa
Collin Jones <collin.g.jones34@gmail.com>
2022-04-19 20:54:16 -0500

    wrote down the relationship lecture code

--- src/main/java/com/example/restblog/data/User.java
+++ src/main/java/com/example/restblog/data/User.java
@@ -21,5 +21,4 @@ public class User {
     private LocalDate createdAt;
     private Role role;
     private Collection<Post> posts;
-
 }
--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,30 +1,38 @@
 package com.example.restblog.web;
 
-
+import com.example.restblog.data.Category;
 import com.example.restblog.data.Post;
-
+import com.example.restblog.data.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostsController {
+    private static final User USER1 = new User(1L, "User 1", "user1@bob.com", "1111", null, User.Role.USER, null);
+    private static final User USER2 = new User(2L, "User 2", "user2@bob.com", "2222", null, User.Role.ADMIN, null);
+    private static final User USER3 = new User(3L, "User 3", "user3@bob.com", "3333", null, User.Role.USER, null);
 
+    private static final Category CAT1 = new Category(1L, "CAT 1", null);
+    private static final Category CAT2 = new Category(2L, "CAT 2", null);
+    private static final Category CAT3 = new Category(3L, "CAT 3", null);
     @GetMapping
     private List<Post> getAll() {
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "hello"));
-        posts.add(new Post(2L, "Post 2", "hello world"));
-        posts.add(new Post(3L, "Post 3", "hello world 2.0"));
+        posts.add(new Post(1L, "Post 1", "hello",USER1, Arrays.asList(CAT1, CAT2)));
+        posts.add(new Post(2L, "Post 2", "hello world",USER2, Arrays.asList(CAT2, CAT3)));
+        posts.add(new Post(3L, "Post 3", "hello world 2.0", USER1, Arrays.asList(CAT1, CAT3)));
         return posts;
     }
 
     @GetMapping("{postId}")
     public Post getById(@PathVariable Long postId){
-        return new Post(postId, "Post" + postId, "hello world");
+        Post post = new Post(postId, "Post " + postId, "This is a test post", USER3, Arrays.asList(CAT1, CAT2));
+        return post;
     }
 
     @PostMapping

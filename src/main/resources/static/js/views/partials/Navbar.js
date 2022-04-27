0c8f798060b6657cef36d56945cb08218c56a12c
Collin Jones <collin.g.jones34@gmail.com>
2022-04-27 10:32:24 -0500

    added some nav bar stuff

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -1,13 +1,24 @@
 export default function Navbar(props) {
     return `
-        <nav>
-            <a href="/" data-link>Home</a>
-            <a href="/posts" data-link>Posts</a>
-            <a href="/about" data-link>About</a>
-            <a href="/login" data-link>Login</a>
-            <a href="/register" data-link>Register</a>
-            <a href="/profile" data-link>Profile</a>
-            <a href="/logout" data-link>Logout</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+    <a class="navbar-brand" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav"></div>
+            
+            <a class="navbar-brand"href="/posts" data-link>Posts</a>
+            <a class="navbar-brand" href="/about" data-link>About</a>
+            <a class="navbar-brand" href="/login" data-link>Login</a>
+            <a class="navbar-brand" href="/register" data-link>Register</a>
+            <a class="navbar-brand" href="/profile" data-link>Profile</a>
+            <a class="navbar-brand" href="/logout" data-link>Logout</a>
+            </div>
+            </div>
+            </div>
         </nav>
+       
     `;
 }
